0ab2611f74397e3255c7a6a43e8237209c1bcc1f
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2018-12-13 20:13:10 +0100

    Product model

--- src/Product.Api.Model/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Product.Api.Model
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Product.Api.Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Product.Api.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public void UpdateWith(Product product)
+        {
+            Name = product.Name;
+            Price = product.Price;
+        }
+    }
+}
